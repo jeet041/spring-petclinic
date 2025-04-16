cdd16c7fd7a5960afb05c0c1151f4cb36b7f81fe
jeet041 <mahorjitendra@gmail.com>
2025-04-16 23:06:03 +0530

    Update VetController.java
    Signed-off-by: jeet041 <mahorjitendra@gmail.com>

--- src/main/java/org/springframework/samples/petclinic/vet/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/vet/VetController.java
@@ -81,6 +81,7 @@ class VetController {
 
     return viewName;
 }
+	
 
 	private String addPaginationModel(int page, Page<Vet> paginated, Model model) {
 		List<Vet> listVets = paginated.getContent();
